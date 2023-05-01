2073a1797cfa0aec1e8bf867857312a566eab0ef
sandhya <sandhya826438@gmail.com>
2023-05-01 16:07:10 +0530

    login page

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <div class="col-sm-4 col-sm-offset-4">
+        <div class="panel panel-default">
+            {{-- <div class="panel-body">
+        Panel content
+    </div> --}}
+            <div class="panel-footer">Copyright©2023 All rights reserved | Designed by Sandhya</div>
+        </div>
+
+    </div>
+</div>
