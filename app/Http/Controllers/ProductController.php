2c5604b3b760eba02656bb4969ce005aeda52022
sandhya <sandhya826438@gmail.com>
2023-05-03 13:43:32 +0530

    Login and logout done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
@@ -28,4 +30,29 @@ class ProductController extends Controller
        where('name', 'like',  '%'.$request->input('query').'%')->get();
         return view('search', ['products' => $data]);
     }
+
+    public function addToCart(Request $request)
+    {
+        if ($request->session()->has('user')) 
+        {
+            $cart = new Cart;
+            $cart->user_id = $request->session()->get('user')['id'];
+            $cart->product_id = $request->product_id;
+            $cart->save();
+            return redirect('/');
+
+        } 
+        else{
+            return redirect('/login');
+        
+        }
+       
+    }
+
+    static function cartItem()
+    {
+        $userId=Session::get('user')['id'];
+        return Cart::where('user_id', $userId)->count();
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SliderController;
+use Illuminate\Support\Facades\Session;
 
 
 /*
@@ -21,8 +22,14 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('login');
+});
+
 Route::post('/login', [UserController::class, 'login']);
 Route::get('/', [ProductController::class, 'index']);
 // Route::get('/', [SliderController::class, 'slider']);
 Route::get('detail/{id}', [ProductController::class, 'detail']);
 Route::get('search', [ProductController::class, 'search']);
+Route::post('add_to_cart', [ProductController::class, 'addToCart']);
