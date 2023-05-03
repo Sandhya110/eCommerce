1e0fae74002085e2560de5fcadcf05a0bd8665c2
sandhya <sandhya826438@gmail.com>
2023-05-03 16:00:13 +0530

    cart module done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+
 
 class ProductController extends Controller
 {
@@ -54,5 +56,25 @@ class ProductController extends Controller
         $userId=Session::get('user')['id'];
         return Cart::where('user_id', $userId)->count();
     }
+
+    public function cartlist()
+    {
+        $userId = Session::get('user')['id'];
+        $products = DB::table('cart')
+            ->join('products', 'cart.product_id', '=', 'products.id')
+            ->where('cart.user_id', $userId)
+            ->select('products.*','cart.id as cart_id')
+            ->get();
+
+        return view('cartlist', ['products' => $products]);
+
+    }
+
+    public function removeCart($id)
+    {
+        Cart::destroy($id);
+        return redirect('cartlist');
+    }
+
     
 }
---
+++ resources/views/cartlist.blade.php
@@ -0,0 +1,39 @@
+@extends('master')
+@section('content')
+    <div class="custom-product">
+        <div class="col-sm-10">
+            <div class="trending-wrapper">
+                <h4>Result for Products</h4>
+                @foreach ($products as $item)
+                    <div class="row searched-item cart-list-divider">
+                        <div class="col-sm-3">
+                            <a href="detail/{{ $item->id }}">
+                                <img class="trending-image" src="{{ $item->gallery }}">
+                            </a>
+                        </div>
+                        <div class="col-sm-3">
+                            <a href="detail/{{ $item->id }}">
+
+                                <div class="">
+                                    <h4>{{ $item->name }}</h4>
+                                    <h5>{{ $item->description }}</h5>
+
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-3">
+                            <a href="/removecart/{{ $item->cart_id }}" class="btn btn-warning">Remove to Cart</a>
+                        </div>
+
+
+                    </div>
+                @endforeach
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+@endsection
